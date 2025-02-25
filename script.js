b27d0532138fc1c4c09ec50e3011c4fa594833f3
Reed Willis <142549714+ReedWillis@users.noreply.github.com>
2025-02-24 23:11:54 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  console.log("doc is ready");
+  
+  console.log("easter eggs are worth +1 points for sure!")
+  
+  
+  console.log("I bark but I am not a dog, I swim and dive through fog. With flippers I glide, and on rocks I lie, What am I, as the waves roll by?  ... think on it ... I AM A SEAL!🦭🦭")
+ });
